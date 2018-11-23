ba365b11e7438260d86b29335caf328f10519638
Trang Trung Hoàng Phúc <trangtrunghoangphuc@gmail.com>
2018-11-23 22:28:43 +0700

    Add files via upload

--- Logic.py
+++ Logic.py
@@ -24,8 +24,12 @@ class Statement:
         self.list_of_terms = []
         for term in list_of_terms:
             self.list_of_terms.append(Term(term.term))
-        self.predicate = predicate
         self.negative = negative
+        if predicate.startswith('\+'):
+            predicate = predicate[2:]
+            self.negative = not self.negative
+        self.predicate = predicate
+        
     def negate(self):
         return Statement(list_of_terms=self.list_of_terms, predicate=self.predicate, negative = not self.negative)
     def add_new_term(self,term):
@@ -638,52 +642,72 @@ class KnowledgeBase:
                         list_of_binding = list_of_binding.merge_or(subgoal_bind)
         return list_of_binding                   
         
-
-
-        
+input_file = ''
+output_file = ''
+inference_choice = 0
+if len(sys.argv) == 2:
+    input_file = sys.argv[1]
+if len(sys.argv) == 3:
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+if input_file =='':
+    input_file = input('Input file: ')
+if output_file == '':
+    output_file = input('Output file: ')
 
 knowledge_base = KnowledgeBase()
-knowledge_base.input_from_file('input.txt')
-# print(str(knowledge_base))
-start = time.time()
+knowledge_base.input_from_file(input_file)
 
-# # Forward chaining
-# for query in knowledge_base.list_of_query:
-#     print('?- ' + str(query))
-#     binding = knowledge_base.forward_chaining(query)
-#     if binding == False:
-#         print('False')
-#     else:
-#         for term in query.list_of_terms:
-#             if term.is_var:
-#                 print(term.term + ' = ' + binding.binding_dict[term.term])
-#         print('True')
-
-# # Backward chaining
-for query in knowledge_base.list_of_query:
-    print('?- ' + str(query))
-    binding_list = knowledge_base.backward_chaining_ask(query)
-    is_fail = True
-    for binding in binding_list.binding_list:
-        if binding.is_fail == False:
-            is_fail = False
+while inference_choice <= 0 or inference_choice >=4:
+    inference_choice = int(input('1. Forward chaining \t 2. Backward chaining \t 3. Resolution \nChoose inference: '))
+f = open(output_file,'w')
+if inference_choice == 1:
+    
+    # Forward chaining
+    for query in knowledge_base.list_of_query:
+        print('?- ' + str(query))
+        f.write('?- ' + str(query) + '\n')
+        binding = knowledge_base.forward_chaining(query)
+        if binding == False:
+            print('False')
+            f.write('False\n')
+        else:
             for term in query.list_of_terms:
                 if term.is_var:
                     print(term.term + ' = ' + binding.binding_dict[term.term])
-    print(not is_fail)
-
-# Resolution
-# query_binding = knowledge_base.resolution()
-# for index in range(len(knowledge_base.list_of_query)):
-#     print('?- ' + str(knowledge_base.list_of_query[index]))
-#     list_of_binding = query_binding[index]
-#     is_fail = True
-#     for binding in list_of_binding.binding_list:
-#         if not binding.is_fail:
-#             is_fail = False
-#             for term in knowledge_base.list_of_query[index].list_of_terms:
-#                 if term.is_var:
-#                     print(term.term + ' = ' + binding.binding_dict[term.term])
-#     print(not is_fail)
-end = time.time()
-print(str(end - start))
+                    f.write(term.term + ' = ' + binding.binding_dict[term.term] + '\n')
+            print('True')
+            f.write('True\n')
+elif inference_choice == 2:
+    # Backward chaining
+    for query in knowledge_base.list_of_query:
+        print('?- ' + str(query))
+        f.write('?- ' + str(query) + '\n')
+        binding_list = knowledge_base.backward_chaining_ask(query)
+        is_fail = True
+        for binding in binding_list.binding_list:
+            if binding.is_fail == False:
+                is_fail = False
+                for term in query.list_of_terms:
+                    if term.is_var:
+                        print(term.term + ' = ' + binding.binding_dict[term.term])
+                        f.write(term.term + ' = ' + binding.binding_dict[term.term] + '\n')
+        f.write(str(not is_fail) + '\n\n')
+elif inference_choice == 3:
+    # Resolution
+    query_binding = knowledge_base.resolution()
+    for index in range(len(knowledge_base.list_of_query)):
+        print('?- ' + str(knowledge_base.list_of_query[index]))
+        f.write('?- ' + str(knowledge_base.list_of_query[index]) + '\n')
+        list_of_binding = query_binding[index]
+        is_fail = True
+        for binding in list_of_binding.binding_list:
+            if not binding.is_fail:
+                is_fail = False
+                for term in knowledge_base.list_of_query[index].list_of_terms:
+                    if term.is_var:
+                        print(term.term + ' = ' + binding.binding_dict[term.term])
+                        f.write(term.term + ' = ' + binding.binding_dict[term.term] + '\n')
+        print(not is_fail)
+        print()
+        f.write(str(not is_fail) + '\n\n')
